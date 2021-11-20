55bd9392c4feef9ae0ec08b41790212ff8a10972
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2021-11-20 15:14:00 +0100

    m3d_camera: change interface

--- m3d_camera.cpp
+++ m3d_camera.cpp
@@ -24,6 +24,12 @@ m3d_camera::m3d_camera(const struct m3d_input_point &position,
     screen_resolution.y = yres;
 }
 
+void m3d_camera::transform_to_frustum(m3d_point &point)
+{
+    transform.transform(point, point);
+    frustum.transform(point, point);
+}
+
 void m3d_camera::transform_to_viewpoint(m3d_point &pointsrc, m3d_point &pointdst)
 {
     transform.transform(pointsrc, pointdst);
@@ -35,23 +41,18 @@ void m3d_camera::transform_to_viewpoint(m3d_vertex &vertex)
     transform.rotate(vertex.normal, vertex.normal);
 }
 
-void m3d_camera::project_to_viewplane(m3d_point &point)
-{
-    frustum.transform(point, point);
-}
-
 void m3d_camera::project_to_screen(m3d_point &point, SDL_Point &pix)
 {
     pix.x = (point.myvector[X_C] / point.myvector[T_C] * screen_resolution.x + screen_resolution.x) / 2;
     pix.y = (-point.myvector[Y_C] / point.myvector[T_C] * screen_resolution.y + screen_resolution.y) / 2;
 }
 
-void m3d_camera::transform_and_project_to_screen(m3d_point &point, SDL_Point &pix)
+void m3d_camera::transform_and_project_to_screen(m3d_point &pointsrc, m3d_point &pointdst, SDL_Point &pix)
 {
-    transform.transform(point, point);
-    frustum.transform(point, point);
-    pix.x = (point.myvector[X_C] / point.myvector[T_C] * screen_resolution.x + screen_resolution.x) / 2;
-    pix.y = (-point.myvector[Y_C] / point.myvector[T_C] * screen_resolution.y + screen_resolution.y) / 2;
+    transform.transform(pointsrc, pointdst);
+    frustum.transform(pointdst, pointdst);
+    pix.x = (pointdst.myvector[X_C] / pointdst.myvector[T_C] * screen_resolution.x + screen_resolution.x) / 2;
+    pix.y = (-pointdst.myvector[Y_C] / pointdst.myvector[T_C] * screen_resolution.y + screen_resolution.y) / 2;
 }
 
 bool m3d_camera::is_visible(m3d_point &point, m3d_vector &normal)
--- m3d_camera.hh
+++ m3d_camera.hh
@@ -7,7 +7,8 @@
 #include "m3d_math.hh"
 #include "m3d_vertex.hh"
 
-struct m3d_screen_point {
+struct m3d_screen_point
+{
     int16_t x;
     int16_t y;
 };
@@ -18,35 +19,34 @@ public:
     /** Default constructor */
     m3d_camera();
     /** Default destructor */
-    virtual ~m3d_camera();
+    ~m3d_camera();
 
-    m3d_camera (const m3d_camera& other) :
-        position(other.position),
-        transform(other.transform),
-        frustum(other.frustum),
-         screen_resolution(other.screen_resolution) {};
+    m3d_camera(const m3d_camera &other) : position(other.position),
+                                          transform(other.transform),
+                                          frustum(other.frustum),
+                                          screen_resolution(other.screen_resolution){};
 
     m3d_camera(const struct m3d_input_point &position,
                const struct m3d_input_point &at,
                int16_t xres,
                int16_t yres);
 
+    // Transform point from world coordinates to view (camera) frustum coordinates
+    void transform_to_frustum(m3d_point &point);
     // Transform point from world coordinates to camera coordinates
-    void transform_to_viewpoint(m3d_point& pointsrc, m3d_point& pointdst);
+    void transform_to_viewpoint(m3d_point &pointsrc, m3d_point &pointdst);
     // Transform vertex from world coordinates to camera coordinates
-    void transform_to_viewpoint(m3d_vertex& vertexsrc);
-    // Project point from camera coordinates to the view plane (frustum near plane)
-    void project_to_viewplane(m3d_point& point);
+    void transform_to_viewpoint(m3d_vertex &vertexsrc);
     // Project point from view plane to screen
-    void project_to_screen(m3d_point& point, SDL_Point& pix);
+    void project_to_screen(m3d_point &point, SDL_Point &pix);
     // Transform point from world coordinates to camera and project to screen
-    void transform_and_project_to_screen(m3d_point& point, SDL_Point& pix);
+    void transform_and_project_to_screen(m3d_point &pointsrc, m3d_point &pointdst, SDL_Point &pix);
     // Compute visibility using normal as a surface normal vector and point to compute
     // the vector going from camera to point itself
-    bool is_visible(m3d_point& point, m3d_vector& normal);
+    bool is_visible(m3d_point &point, m3d_vector &normal);
 
-    const m3d_point &get_position(void) { return  position; }
-protected:
+    const m3d_point &get_position(void) { return position; }
+
 private:
     m3d_point position;
     m3d_matrix_camera transform;
