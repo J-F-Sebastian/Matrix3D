a763a51dbc5be65ab80259981128c4f757d9035c
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2024-05-31 20:40:23 +0200

    m3d_color: run indentation

--- m3d_color.cpp
+++ m3d_color.cpp
@@ -7,103 +7,111 @@ using namespace std;
 
 void m3d_color::change_channel_int(uint8_t &chan, float intensity)
 {
-    int temp;
+	int temp;
 
-    temp = (int)((float)chan * intensity);
+	temp = (int)((float)chan * intensity);
 
-    temp = (temp < 0) ? 0 : (temp > 255) ? 255 : temp;
-    
-    chan = (uint8_t) temp;
+	temp = (temp < 0) ? 0 : (temp > 255) ? 255
+					     : temp;
+
+	chan = (uint8_t)temp;
 }
 
 void m3d_color::setColor(uint8_t red,
-                         uint8_t green,
-                         uint8_t blue,
-                         uint8_t alpha)
+			 uint8_t green,
+			 uint8_t blue,
+			 uint8_t alpha)
 {
-    mycolor.channels[R_CHANNEL] = red;
-    mycolor.channels[G_CHANNEL] = green;
-    mycolor.channels[B_CHANNEL] = blue;
-    mycolor.channels[A_CHANNEL] = alpha;
+	mycolor.channels[R_CHANNEL] = red;
+	mycolor.channels[G_CHANNEL] = green;
+	mycolor.channels[B_CHANNEL] = blue;
+	mycolor.channels[A_CHANNEL] = alpha;
 }
 
 void m3d_color::brighten(float intensity)
 {
-    if (std::signbit(intensity)) {
-        intensity = 0.0f;
-    }
+	if (std::signbit(intensity))
+	{
+		intensity = 0.0f;
+	}
 
-    change_channel_int(mycolor.channels[R_CHANNEL], intensity);
-    change_channel_int(mycolor.channels[G_CHANNEL], intensity);
-    change_channel_int(mycolor.channels[B_CHANNEL], intensity);
+	change_channel_int(mycolor.channels[R_CHANNEL], intensity);
+	change_channel_int(mycolor.channels[G_CHANNEL], intensity);
+	change_channel_int(mycolor.channels[B_CHANNEL], intensity);
 }
 
 uint32_t m3d_color::brighten2(float intensity)
 {
-    m3d_color_channels myunion;
+	m3d_color_channels myunion;
 
-     if (std::signbit(intensity)) {
-        intensity = 0.0f;
-    }
+	if (std::signbit(intensity))
+	{
+		intensity = 0.0f;
+	}
 
-    myunion.color = mycolor.color;
-    change_channel_int(myunion.channels[R_CHANNEL], intensity);
-    change_channel_int(myunion.channels[G_CHANNEL], intensity);
-    change_channel_int(myunion.channels[B_CHANNEL], intensity);
+	myunion.color = mycolor.color;
+	change_channel_int(myunion.channels[R_CHANNEL], intensity);
+	change_channel_int(myunion.channels[G_CHANNEL], intensity);
+	change_channel_int(myunion.channels[B_CHANNEL], intensity);
 
-    return (myunion.color);
+	return (myunion.color);
 }
 
 void m3d_color::brighten_channels(float chansint[])
 {
-    for (unsigned i = 0; i < A_CHANNEL; i++) {
-        if (std::signbit(chansint[i])) {
-            chansint[i] = 0.0f;
-        }
-    }
+	for (unsigned i = 0; i < A_CHANNEL; i++)
+	{
+		if (std::signbit(chansint[i]))
+		{
+			chansint[i] = 0.0f;
+		}
+	}
 
-    change_channel_int(mycolor.channels[R_CHANNEL], chansint[R_CHANNEL]);
-    change_channel_int(mycolor.channels[G_CHANNEL], chansint[G_CHANNEL]);
-    change_channel_int(mycolor.channels[B_CHANNEL], chansint[B_CHANNEL]);
+	change_channel_int(mycolor.channels[R_CHANNEL], chansint[R_CHANNEL]);
+	change_channel_int(mycolor.channels[G_CHANNEL], chansint[G_CHANNEL]);
+	change_channel_int(mycolor.channels[B_CHANNEL], chansint[B_CHANNEL]);
 }
 
 uint32_t m3d_color::brighten_channels2(float chansint[])
 {
-    m3d_color_channels myunion;
+	m3d_color_channels myunion;
 
-    for (unsigned i = 0; i < A_CHANNEL; i++) {
-        if (std::signbit(chansint[i])) {
-            chansint[i] = 0.0f;
-        }
-    }
+	for (unsigned i = 0; i < A_CHANNEL; i++)
+	{
+		if (std::signbit(chansint[i]))
+		{
+			chansint[i] = 0.0f;
+		}
+	}
 
-    myunion.color = mycolor.color;
-    change_channel_int(myunion.channels[R_CHANNEL], chansint[R_CHANNEL]);
-    change_channel_int(myunion.channels[G_CHANNEL], chansint[G_CHANNEL]);
-    change_channel_int(myunion.channels[B_CHANNEL], chansint[B_CHANNEL]);
-    return (myunion.color);
+	myunion.color = mycolor.color;
+	change_channel_int(myunion.channels[R_CHANNEL], chansint[R_CHANNEL]);
+	change_channel_int(myunion.channels[G_CHANNEL], chansint[G_CHANNEL]);
+	change_channel_int(myunion.channels[B_CHANNEL], chansint[B_CHANNEL]);
+	return (myunion.color);
 }
 
 void m3d_color::average_colors(m3d_color array[], unsigned num, m3d_color &out)
 {
-    unsigned sumr = 0, sumg = 0, sumb = 0;
-    unsigned count = num;
+	unsigned sumr = 0, sumg = 0, sumb = 0;
+	unsigned count = num;
 
-    while (count--) {
-        sumr += array[count].getColor() & 0xFF;
-        sumg += (array[count].getColor() >> 8) & 0xFF;
-        sumb += (array[count].getColor() >> 16) & 0xFF;
-    }
+	while (count--)
+	{
+		sumr += array[count].getColor() & 0xFF;
+		sumg += (array[count].getColor() >> 8) & 0xFF;
+		sumb += (array[count].getColor() >> 16) & 0xFF;
+	}
 
-    sumr /= num;
-    sumg /= num;
-    sumb /= num;
+	sumr /= num;
+	sumg /= num;
+	sumb /= num;
 
-    sumr %= 256;
-    sumg %= 256;
-    sumb %= 256;
+	sumr %= 256;
+	sumg %= 256;
+	sumb %= 256;
 
-    out.setColor(sumr, sumg, sumb, 0);
+	out.setColor(sumr, sumg, sumb, 0);
 }
 
 /*
@@ -113,34 +121,35 @@ void m3d_color::average_colors(m3d_color array[], unsigned num, m3d_color &out)
  */
 void m3d_color::add_colors(m3d_color *array[], unsigned num, m3d_color &out)
 {
-    unsigned sumr = 0, sumg = 0, sumb = 0;
+	unsigned sumr = 0, sumg = 0, sumb = 0;
 
-    while (num--) {
-        sumr += (*array)->getColor() & 0xFF;
-        sumg += ((*array)->getColor() >> 8) & 0xFF;
-        sumb += ((*array)->getColor() >> 16) & 0xFF;
-        array++;
-    }
+	while (num--)
+	{
+		sumr += (*array)->getColor() & 0xFF;
+		sumg += ((*array)->getColor() >> 8) & 0xFF;
+		sumb += ((*array)->getColor() >> 16) & 0xFF;
+		array++;
+	}
 
-    sumr %= 256;
-    sumg %= 256;
-    sumb %= 256;
+	sumr %= 256;
+	sumg %= 256;
+	sumb %= 256;
 
-    out.setColor(sumr, sumg, sumb, 0);
+	out.setColor(sumr, sumg, sumb, 0);
 }
 
 void m3d_color::print()
 {
-#ifdef NDEBUG    
-    cout << "Color RGBA ("
-         << (int)mycolor.channels[R_CHANNEL]
-         << ","
-         << (int)mycolor.channels[G_CHANNEL]
-         << ","
-         << (int)mycolor.channels[B_CHANNEL]
-         << ","
-         << (int)mycolor.channels[A_CHANNEL]
-         << ")"
-         << endl;
+#ifdef NDEBUG
+	cout << "Color RGBA ("
+	     << (int)mycolor.channels[R_CHANNEL]
+	     << ","
+	     << (int)mycolor.channels[G_CHANNEL]
+	     << ","
+	     << (int)mycolor.channels[B_CHANNEL]
+	     << ","
+	     << (int)mycolor.channels[A_CHANNEL]
+	     << ")"
+	     << endl;
 #endif
 }
--- m3d_color.hh
+++ m3d_color.hh
@@ -6,125 +6,128 @@
 class m3d_color
 {
 public:
-    enum {
-        B_CHANNEL,
-        G_CHANNEL,
-        R_CHANNEL,
-        A_CHANNEL
-    };
+	enum
+	{
+		B_CHANNEL,
+		G_CHANNEL,
+		R_CHANNEL,
+		A_CHANNEL
+	};
 
-    enum {
-        B_SHIFT = 0,
-        G_SHIFT = 8,
-        R_SHIFT = 16,
-        A_SHIFT = 24
-    };
+	enum
+	{
+		B_SHIFT = 0,
+		G_SHIFT = 8,
+		R_SHIFT = 16,
+		A_SHIFT = 24
+	};
 
-    union m3d_color_channels {
-        uint32_t color;
-        uint8_t channels[4];
-    };
+	union m3d_color_channels
+	{
+		uint32_t color;
+		uint8_t channels[4];
+	};
 
-    /** Default constructor */
-    m3d_color()
-    {
-        mycolor.color = 0;
-    };
+	/** Default constructor */
+	m3d_color()
+	{
+		mycolor.color = 0;
+	};
 
-    /** Default destructor */
-    ~m3d_color() {};
+	/** Default destructor */
+	~m3d_color(){};
 
-    /** Copy constructor
-     *  \param other Object to copy from
-     */
-    m3d_color(const m3d_color& other) : mycolor(other.mycolor) {};
+	/** Copy constructor
+	 *  \param other Object to copy from
+	 */
+	m3d_color(const m3d_color &other) : mycolor(other.mycolor){};
 
-    m3d_color(uint32_t color)
-    {
-        mycolor.color = color;
-    };
+	m3d_color(uint32_t color)
+	{
+		mycolor.color = color;
+	};
 
-    m3d_color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha)
-    {
-        setColor(red,green,blue,alpha);
-    }
+	m3d_color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha)
+	{
+		setColor(red, green, blue, alpha);
+	}
 
-    void operator = (const m3d_color& other)
-    {
-        mycolor.color = other.mycolor.color;
-    }
+	void operator=(const m3d_color &other)
+	{
+		mycolor.color = other.mycolor.color;
+	}
 
-    m3d_color operator * (const m3d_color& other)
-    {
-        unsigned tempc;
+	m3d_color operator*(const m3d_color &other)
+	{
+		unsigned tempc;
 
-        m3d_color temp(*this);
-        tempc = temp.mycolor.channels[R_CHANNEL]*other.mycolor.channels[R_CHANNEL];
-        tempc >>= 8;
-        temp.mycolor.channels[R_CHANNEL] = tempc;
-        tempc = temp.mycolor.channels[G_CHANNEL]*other.mycolor.channels[G_CHANNEL];
-        tempc >>= 8;
-        temp.mycolor.channels[G_CHANNEL] = tempc;
-        tempc = temp.mycolor.channels[B_CHANNEL]*other.mycolor.channels[B_CHANNEL];
-        tempc >>= 8;
-        temp.mycolor.channels[B_CHANNEL] = tempc;
-        return temp;
-    }
+		m3d_color temp(*this);
+		tempc = temp.mycolor.channels[R_CHANNEL] * other.mycolor.channels[R_CHANNEL];
+		tempc >>= 8;
+		temp.mycolor.channels[R_CHANNEL] = tempc;
+		tempc = temp.mycolor.channels[G_CHANNEL] * other.mycolor.channels[G_CHANNEL];
+		tempc >>= 8;
+		temp.mycolor.channels[G_CHANNEL] = tempc;
+		tempc = temp.mycolor.channels[B_CHANNEL] * other.mycolor.channels[B_CHANNEL];
+		tempc >>= 8;
+		temp.mycolor.channels[B_CHANNEL] = tempc;
+		return temp;
+	}
 
-    /*
-     *  setColor fills color with the proper values
-     */
-    void setColor(uint8_t red,
-                  uint8_t green,
-                  uint8_t blue,
-                  uint8_t alpha);
+	/*
+	 *  setColor fills color with the proper values
+	 */
+	void setColor(uint8_t red,
+		      uint8_t green,
+		      uint8_t blue,
+		      uint8_t alpha);
 
-    uint32_t getColor(void)
-    {
-        return (mycolor.color);
-    }
+	uint32_t getColor(void)
+	{
+		return (mycolor.color);
+	}
 
-    uint8_t getChannel(unsigned chanNum)
-    {
-        return mycolor.channels[chanNum];
-    }
+	uint8_t getChannel(unsigned chanNum)
+	{
+		return mycolor.channels[chanNum];
+	}
 
-    /*
-     * void brighten applies a new intensity to color; uint32_t brighten2
-     * compute and return a copy of color, does not modify the stored value
-     */
-    void brighten(float intensity);
-    uint32_t brighten2(float intensity);
+	/*
+	 * void brighten applies a new intensity to color; uint32_t brighten2
+	 * compute and return a copy of color, does not modify the stored value
+	 */
+	void brighten(float intensity);
+	uint32_t brighten2(float intensity);
 
-    /*
-     * void brighten_channels applies a new intensity to the channels in color,
-     * chansint must point to an array of 3 floats for intensities.
-     * uint32_t brighten_channels compute and return a copy of color, does not modify
-     * the stored value.
-     */
-    void brighten_channels(float *chansint);
-    uint32_t brighten_channels2(float *chansint);
+	/*
+	 * void brighten_channels applies a new intensity to the channels in color,
+	 * chansint must point to an array of 3 floats for intensities.
+	 * uint32_t brighten_channels compute and return a copy of color, does not modify
+	 * the stored value.
+	 */
+	void brighten_channels(float *chansint);
+	uint32_t brighten_channels2(float *chansint);
 
-    /*
-     * average_colors compute the average color for num items in array and
-     * store the resulting value in out.
-     * Average is not weighted, i.e. all colors contribute equally to the average.
-     */
-    static void average_colors(m3d_color array[], unsigned num, m3d_color &out);
+	/*
+	 * average_colors compute the average color for num items in array and
+	 * store the resulting value in out.
+	 * Average is not weighted, i.e. all colors contribute equally to the average.
+	 */
+	static void average_colors(m3d_color array[], unsigned num, m3d_color &out);
 
-    /*
-     * add_colors compute the sum for num items in array and store the result
-     * in out.
-     * Sums for R, G, B channels are clamped to 255.
-     */
-    static void add_colors(m3d_color *array[], unsigned num, m3d_color &out);
+	/*
+	 * add_colors compute the sum for num items in array and store the result
+	 * in out.
+	 * Sums for R, G, B channels are clamped to 255.
+	 */
+	static void add_colors(m3d_color *array[], unsigned num, m3d_color &out);
 
-    void print(void);
+	void print(void);
 
 protected:
 private:
-    inline void change_channel_int(uint8_t &chan, float intensity);
-    union m3d_color_channels mycolor;
+	inline void change_channel_int(uint8_t &chan, float intensity);
+	union m3d_color_channels mycolor;
 };
 
 #endif // M3D_COLOR_H
