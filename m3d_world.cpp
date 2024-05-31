2ee6725c6681dbc3b092d08381d76904e5a51fae
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2024-05-31 20:39:08 +0200

    m3d_world: run indentation

--- m3d_world.cpp
+++ m3d_world.cpp
@@ -5,86 +5,86 @@
 using namespace std;
 
 static bool zsortobjects(const m3d_render_object *first,
-                         const m3d_render_object *second)
+			 const m3d_render_object *second)
 {
-    return (first->z_sorting < second->z_sorting) ? true : false;
+	return (first->z_sorting < second->z_sorting) ? true : false;
 }
 
 m3d_world::~m3d_world()
 {
-    objects_list.clear();
-    lights_list.clear();
+	objects_list.clear();
+	lights_list.clear();
 }
 
 int m3d_world::add_object(m3d_render_object &object)
 {
-    try
-    {
-        objects_list.push_back(&object);
-    }
-    catch (const bad_alloc &e)
-    {
-        cerr << "Exception: " << e.what() << endl;
-        return (ENOMEM);
-    }
+	try
+	{
+		objects_list.push_back(&object);
+	}
+	catch (const bad_alloc &e)
+	{
+		cerr << "Exception: " << e.what() << endl;
+		return (ENOMEM);
+	}
 
-    return (0);
+	return (0);
 }
 
 int m3d_world::add_light_source(m3d_point_light_source &light_source)
 {
-    try
-    {
-        lights_list.push_back(&light_source);
-    }
-    catch (const bad_alloc &e)
-    {
-        cerr << "Exception: " << e.what() << endl;
-        return (ENOMEM);
-    }
+	try
+	{
+		lights_list.push_back(&light_source);
+	}
+	catch (const bad_alloc &e)
+	{
+		cerr << "Exception: " << e.what() << endl;
+		return (ENOMEM);
+	}
 
-    return (0);
+	return (0);
 }
 
 void m3d_world::set_ambient_light(m3d_ambient_light &ambient_light)
 {
-    this->ambient_light = ambient_light;
+	this->ambient_light = ambient_light;
 }
 
 void m3d_world::set_ambient_light_intensity(float intensity)
 {
-    ambient_light.set_src_intensity(intensity);
+	ambient_light.set_src_intensity(intensity);
 }
 
 void m3d_world::sort(list<m3d_render_object *> &objects_list)
 {
-    list<m3d_render_object *>::iterator itro = objects_list.begin();
-    m3d_point temp;
+	list<m3d_render_object *>::iterator itro = objects_list.begin();
+	m3d_point temp;
 
-    while (itro != objects_list.end())
-    {
-        camera.transform_to_viewpoint((*itro)->center, temp);
-        (*itro)->z_sorting = temp.myvector[Z_C];
-        ++itro;
-    }
-    objects_list.sort(zsortobjects);
+	while (itro != objects_list.end())
+	{
+		camera.transform_to_viewpoint((*itro)->center, temp);
+		(*itro)->z_sorting = temp.myvector[Z_C];
+		++itro;
+	}
+	objects_list.sort(zsortobjects);
 }
 
 void m3d_world::print()
 {
 #ifdef NDEBUG
-    list<m3d_render_object *>::iterator itro;
-    list<m3d_point_light_source *>::iterator itls;
+	list<m3d_render_object *>::iterator itro;
+	list<m3d_point_light_source *>::iterator itls;
 
-    cout << "World" << endl;
-    for (itro = objects_list.begin(); itro != objects_list.end(); itro++)
-    {
-        (*itro)->print();
-    }
+	cout << "World" << endl;
+	for (itro = objects_list.begin(); itro != objects_list.end(); itro++)
+	{
+		(*itro)->print();
+	}
 
-    for (itls = lights_list.begin(); itls != lights_list.end(); itls++)
-    {
-        (*itls)->print();
-    }
+	for (itls = lights_list.begin(); itls != lights_list.end(); itls++)
+	{
+		(*itls)->print();
+	}
 #endif
 }
--- m3d_world.hh
+++ m3d_world.hh
@@ -10,25 +10,25 @@
 class m3d_world
 {
 public:
-    /** Default constructor */
-    m3d_world() : objects_list(), lights_list(), ambient_light(), camera(){};
-    /** Default destructor */
-    ~m3d_world();
+	/** Default constructor */
+	m3d_world() : objects_list(), lights_list(), ambient_light(), camera(){};
+	/** Default destructor */
+	~m3d_world();
 
-    m3d_world(const m3d_ambient_light &ambient, const m3d_camera &camera) : objects_list(), lights_list(), ambient_light(ambient), camera(camera){};
+	m3d_world(const m3d_ambient_light &ambient, const m3d_camera &camera) : objects_list(), lights_list(), ambient_light(ambient), camera(camera){};
 
-    int add_object(m3d_render_object &object);
-    int add_light_source(m3d_point_light_source &light_source);
-    void set_ambient_light(m3d_ambient_light &ambient_light);
-    void set_ambient_light_intensity(float intensity);
+	int add_object(m3d_render_object &object);
+	int add_light_source(m3d_point_light_source &light_source);
+	void set_ambient_light(m3d_ambient_light &ambient_light);
+	void set_ambient_light_intensity(float intensity);
 
-    void sort(std::list<m3d_render_object *> &objects_list);
-    void print(void);
+	void sort(std::list<m3d_render_object *> &objects_list);
+	void print(void);
 
-    std::list<m3d_render_object *> objects_list;
-    std::list<m3d_point_light_source *> lights_list;
-    m3d_ambient_light ambient_light;
-    m3d_camera camera;
+	std::list<m3d_render_object *> objects_list;
+	std::list<m3d_point_light_source *> lights_list;
+	m3d_ambient_light ambient_light;
+	m3d_camera camera;
 };
 
 #endif // M3D_WORLD_H
