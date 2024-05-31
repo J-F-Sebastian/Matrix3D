0a9cbe0fbf504f6ff19b6ef8a2d15ced6853154d
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2024-05-31 20:40:15 +0200

    m3d_display: run indentation

--- m3d_display.cpp
+++ m3d_display.cpp
@@ -6,149 +6,149 @@ using namespace std;
 
 m3d_display::m3d_display(int xres, int yres)
 {
-    SDL_version linked;
-    SDL_DisplayMode mode;
+	SDL_version linked;
+	SDL_DisplayMode mode;
 
-    //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
-    }
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
+	}
 
-    SDL_GetVersion(&linked);
+	SDL_GetVersion(&linked);
 
-    cout << "SDL version is " << (int)linked.major << "." << (int)linked.minor << "." << (int)linked.patch << endl;
+	cout << "SDL version is " << (int)linked.major << "." << (int)linked.minor << "." << (int)linked.patch << endl;
 
-    // create a new window
-    window = SDL_CreateWindow("Matrix3D",
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SDL_WINDOWPOS_UNDEFINED,
-                              xres,
-                              yres,
-                              SDL_WINDOW_SHOWN);
+	// create a new window
+	window = SDL_CreateWindow("Matrix3D",
+				  SDL_WINDOWPOS_UNDEFINED,
+				  SDL_WINDOWPOS_UNDEFINED,
+				  xres,
+				  yres,
+				  SDL_WINDOW_SHOWN);
 
-    if (!window)
-    {
-        cout << "Unable to create " << xres << "x" << yres << " window: " << SDL_GetError() << endl;
-    }
+	if (!window)
+	{
+		cout << "Unable to create " << xres << "x" << yres << " window: " << SDL_GetError() << endl;
+	}
 
-    screenSurface = SDL_GetWindowSurface(window);
+	screenSurface = SDL_GetWindowSurface(window);
 
-    if (!screenSurface)
-    {
-        cout << "Unable to get buffer : " << SDL_GetError() << endl;
-    }
+	if (!screenSurface)
+	{
+		cout << "Unable to get buffer : " << SDL_GetError() << endl;
+	}
 
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    if (!renderer)
-    {
-        cout << "Unable to create accelerated renderer :" << SDL_GetError() << endl;
-        renderer = SDL_CreateSoftwareRenderer(screenSurface);
-    }
+	if (!renderer)
+	{
+		cout << "Unable to create accelerated renderer :" << SDL_GetError() << endl;
+		renderer = SDL_CreateSoftwareRenderer(screenSurface);
+	}
 
-    if (!renderer)
-    {
-        cout << "Unable to create software renderer :" << SDL_GetError() << endl;
-    }
+	if (!renderer)
+	{
+		cout << "Unable to create software renderer :" << SDL_GetError() << endl;
+	}
 
-    if (SDL_GetCurrentDisplayMode(0, &mode))
-    {
-        cout << "Unable to get display mode :" << SDL_GetError() << endl;
-    }
-    else
-    {
-        cout << "Display Resolution " << mode.w << " x " << mode.h << endl
-             << "format: " << endl
-             << "pixel type     : " << SDL_PIXELTYPE(mode.format) << endl
-             << "pixel order    : " << SDL_PIXELORDER(mode.format) << endl
-             << "pixel layout   : " << SDL_PIXELLAYOUT(mode.format) << endl
-             << "bits per pixel : " << SDL_BITSPERPIXEL(mode.format) << endl
-             << "bytes per pixel: " << SDL_BYTESPERPIXEL(mode.format) << endl
-             << "alpha channel  : " << SDL_ISPIXELFORMAT_ALPHA(mode.format) << endl
-             << "format         : " << SDL_GetPixelFormatName(mode.format) << endl
-             << "---------------------------------------" << endl;
-    }
+	if (SDL_GetCurrentDisplayMode(0, &mode))
+	{
+		cout << "Unable to get display mode :" << SDL_GetError() << endl;
+	}
+	else
+	{
+		cout << "Display Resolution " << mode.w << " x " << mode.h << endl
+		     << "format: " << endl
+		     << "pixel type     : " << SDL_PIXELTYPE(mode.format) << endl
+		     << "pixel order    : " << SDL_PIXELORDER(mode.format) << endl
+		     << "pixel layout   : " << SDL_PIXELLAYOUT(mode.format) << endl
+		     << "bits per pixel : " << SDL_BITSPERPIXEL(mode.format) << endl
+		     << "bytes per pixel: " << SDL_BYTESPERPIXEL(mode.format) << endl
+		     << "alpha channel  : " << SDL_ISPIXELFORMAT_ALPHA(mode.format) << endl
+		     << "format         : " << SDL_GetPixelFormatName(mode.format) << endl
+		     << "---------------------------------------" << endl;
+	}
 
-    if (SDL_GetWindowDisplayMode(window, &mode))
-    {
-        cout << "Unable to get window mode :" << SDL_GetError() << endl;
-    }
-    else
-    {
-        cout << "Window Resolution " << mode.w << " x " << mode.h << endl
-             << "format: " << endl
-             << "pixel type     : " << SDL_PIXELTYPE(mode.format) << endl
-             << "pixel order    : " << SDL_PIXELORDER(mode.format) << endl
-             << "pixel layout   : " << SDL_PIXELLAYOUT(mode.format) << endl
-             << "bits per pixel : " << SDL_BITSPERPIXEL(mode.format) << endl
-             << "bytes per pixel: " << SDL_BYTESPERPIXEL(mode.format) << endl
-             << "alpha channel  : " << SDL_ISPIXELFORMAT_ALPHA(mode.format) << endl
-             << "format         : " << SDL_GetPixelFormatName(mode.format) << endl
-             << "---------------------------------------" << endl;
-    }
+	if (SDL_GetWindowDisplayMode(window, &mode))
+	{
+		cout << "Unable to get window mode :" << SDL_GetError() << endl;
+	}
+	else
+	{
+		cout << "Window Resolution " << mode.w << " x " << mode.h << endl
+		     << "format: " << endl
+		     << "pixel type     : " << SDL_PIXELTYPE(mode.format) << endl
+		     << "pixel order    : " << SDL_PIXELORDER(mode.format) << endl
+		     << "pixel layout   : " << SDL_PIXELLAYOUT(mode.format) << endl
+		     << "bits per pixel : " << SDL_BITSPERPIXEL(mode.format) << endl
+		     << "bytes per pixel: " << SDL_BYTESPERPIXEL(mode.format) << endl
+		     << "alpha channel  : " << SDL_ISPIXELFORMAT_ALPHA(mode.format) << endl
+		     << "format         : " << SDL_GetPixelFormatName(mode.format) << endl
+		     << "---------------------------------------" << endl;
+	}
 
-    //This stuff must be reviewed for proper failure handling
-    xmax = mode.w;
-    ymax = mode.h;
+	// This stuff must be reviewed for proper failure handling
+	xmax = mode.w;
+	ymax = mode.h;
 
-    //Fill the surface black
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+	// Fill the surface black
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
 }
 
 /** Default destructor */
 m3d_display::~m3d_display()
 {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 uint32_t *m3d_display::get_video_buffer(int16_t x0, int16_t y0)
 {
-    return (uint32_t *)(screenSurface->pixels) + (y0 * screenSurface->w) + x0;
+	return (uint32_t *)(screenSurface->pixels) + (y0 * screenSurface->w) + x0;
 }
 
 void m3d_display::set_color(uint8_t red, uint8_t green, uint8_t blue)
 {
-    SDL_SetRenderDrawColor(renderer, red, green, blue, SDL_ALPHA_OPAQUE);
+	SDL_SetRenderDrawColor(renderer, red, green, blue, SDL_ALPHA_OPAQUE);
 }
 
 void m3d_display::fill_buffer()
 {
-    SDL_RenderClear(renderer);
+	SDL_RenderClear(renderer);
 }
 
 void m3d_display::show_buffer()
 {
-    //Present the buffer
-    SDL_UpdateWindowSurface(window);
+	// Present the buffer
+	SDL_UpdateWindowSurface(window);
 }
 
 void m3d_display::clear_buffer()
 {
-    SDL_FillRect(screenSurface, &screenSurface->clip_rect, 0);
+	SDL_FillRect(screenSurface, &screenSurface->clip_rect, 0);
 }
 
 void m3d_display::clear_renderer()
 {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
 }
 
 void m3d_display::draw_lines(SDL_Point pts[], unsigned ptsnum)
 {
-    if (SDL_RenderDrawLines(renderer, pts, ptsnum) < 0)
-    {
-        cout << "SDL error: " << SDL_GetError() << endl;
-    }
+	if (SDL_RenderDrawLines(renderer, pts, ptsnum) < 0)
+	{
+		cout << "SDL error: " << SDL_GetError() << endl;
+	}
 }
 
 void m3d_display::show_renderer()
 {
-    //Present the rendered lines
-    SDL_RenderPresent(renderer);
+	// Present the rendered lines
+	SDL_RenderPresent(renderer);
 }
--- m3d_display.hh
+++ m3d_display.hh
@@ -7,37 +7,37 @@
 class m3d_display
 {
 public:
-    /** Default constructor */
-    m3d_display() : window(0), screenSurface(0), renderer(0), xmax(0), ymax(0){};
-    m3d_display(int xres, int yres);
-    /** Default destructor */
-    ~m3d_display();
+	/** Default constructor */
+	m3d_display() : window(0), screenSurface(0), renderer(0), xmax(0), ymax(0){};
+	m3d_display(int xres, int yres);
+	/** Default destructor */
+	~m3d_display();
 
-    uint32_t *get_video_buffer(int16_t x0, int16_t y0);
+	uint32_t *get_video_buffer(int16_t x0, int16_t y0);
 
-    int get_xmax(void) { return xmax; }
-    int get_ymax(void) { return ymax; }
+	int get_xmax(void) { return xmax; }
+	int get_ymax(void) { return ymax; }
 
-    /** Act directly on the surface buffer */
-    void fill_buffer(void);
-    void show_buffer(void);
-    void clear_buffer(void);
+	/** Act directly on the surface buffer */
+	void fill_buffer(void);
+	void show_buffer(void);
+	void clear_buffer(void);
 
-    /** Act on the renderer */
-    void set_color(uint8_t red, uint8_t green, uint8_t blue);
-    void draw_lines(SDL_Point pts[], unsigned ptsnum);
-    void clear_renderer(void);
-    void show_renderer(void);
+	/** Act on the renderer */
+	void set_color(uint8_t red, uint8_t green, uint8_t blue);
+	void draw_lines(SDL_Point pts[], unsigned ptsnum);
+	void clear_renderer(void);
+	void show_renderer(void);
 
 private:
-    //The window we'll be rendering to
-    SDL_Window *window;
-    //The surface contained by the window
-    SDL_Surface *screenSurface;
-    //The SDL renderer
-    SDL_Renderer *renderer;
-    //The window resolution
-    int xmax, ymax;
+	// The window we'll be rendering to
+	SDL_Window *window;
+	// The surface contained by the window
+	SDL_Surface *screenSurface;
+	// The SDL renderer
+	SDL_Renderer *renderer;
+	// The window resolution
+	int xmax, ymax;
 };
 
 #endif
