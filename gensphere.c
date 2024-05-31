603820f5fa94355a00ada7147e8f45c8708b7f01
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2024-05-31 21:44:23 +0200

    gensphere.c: fix indentation and MSVC warnings

--- gensphere.c
+++ gensphere.c
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,261 +8,263 @@ FILE *out;
 
 void output_mesh_down(int startbottom, int starttop, int points)
 {
-    int a, b, c, tricount;
+	int a, b, c, tricount;
 
-    a = starttop;
-    b = startbottom;
-    c = startbottom + 1;
-    tricount = points;
-    while (tricount--)
-    {
-        fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
-        a++;
-        b++;
-        c++;
-        if (a == starttop + points)
-            a = starttop;
-        if (b == startbottom + points)
-            b = startbottom;
-        if (c == startbottom + points)
-            c = startbottom;
-    }
+	a = starttop;
+	b = startbottom;
+	c = startbottom + 1;
+	tricount = points;
+	while (tricount--)
+	{
+		fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
+		a++;
+		b++;
+		c++;
+		if (a == starttop + points)
+			a = starttop;
+		if (b == startbottom + points)
+			b = startbottom;
+		if (c == startbottom + points)
+			c = startbottom;
+	}
 
-    a = starttop;
-    b = startbottom + 1;
-    c = starttop + 1;
-    tricount = points;
-    while (tricount--)
-    {
-        fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
-        a++;
-        b++;
-        c++;
-        if (a == starttop + points)
-            a = starttop;
-        if (b == startbottom + points)
-            b = startbottom;
-        if (c == starttop + points)
-            c = starttop;
-    }
+	a = starttop;
+	b = startbottom + 1;
+	c = starttop + 1;
+	tricount = points;
+	while (tricount--)
+	{
+		fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
+		a++;
+		b++;
+		c++;
+		if (a == starttop + points)
+			a = starttop;
+		if (b == startbottom + points)
+			b = startbottom;
+		if (c == starttop + points)
+			c = starttop;
+	}
 }
 
 void output_mesh_up(int startbottom, int starttop, int points)
 {
-    int a, b, c, tricount;
+	int a, b, c, tricount;
 
-    a = startbottom;
-    b = startbottom + 1;
-    c = starttop;
-    tricount = points;
-    while (tricount--)
-    {
-        fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
-        a++;
-        b++;
-        c++;
-        if (a == startbottom + points)
-            a = startbottom;
-        if (b == startbottom + points)
-            b = startbottom;
-        if (c == starttop + points)
-            c = starttop;
-    }
+	a = startbottom;
+	b = startbottom + 1;
+	c = starttop;
+	tricount = points;
+	while (tricount--)
+	{
+		fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
+		a++;
+		b++;
+		c++;
+		if (a == startbottom + points)
+			a = startbottom;
+		if (b == startbottom + points)
+			b = startbottom;
+		if (c == starttop + points)
+			c = starttop;
+	}
 
-    a = starttop;
-    b = startbottom + 1;
-    c = starttop + 1;
-    tricount = points;
-    while (tricount--)
-    {
-        fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
-        a++;
-        b++;
-        c++;
-        if (a == starttop + points)
-            a = starttop;
-        if (b == startbottom + points)
-            b = startbottom;
-        if (c == starttop + points)
-            c = starttop;
-    }
+	a = starttop;
+	b = startbottom + 1;
+	c = starttop + 1;
+	tricount = points;
+	while (tricount--)
+	{
+		fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
+		a++;
+		b++;
+		c++;
+		if (a == starttop + points)
+			a = starttop;
+		if (b == startbottom + points)
+			b = startbottom;
+		if (c == starttop + points)
+			c = starttop;
+	}
 }
 
 void output_mesh_top(int startbottom, int top, int points)
 {
-    int a, b, c, tricount;
+	int a, b, c, tricount;
 
-    a = startbottom;
-    b = startbottom + 1;
-    c = top;
-    tricount = points;
+	a = startbottom;
+	b = startbottom + 1;
+	c = top;
+	tricount = points;
 
-    while (tricount--)
-    {
-        fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
-        a++;
-        b++;
-        if (a == startbottom + points)
-            a = startbottom;
-        if (b == startbottom + points)
-            b = startbottom;
-    }
+	while (tricount--)
+	{
+		fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
+		a++;
+		b++;
+		if (a == startbottom + points)
+			a = startbottom;
+		if (b == startbottom + points)
+			b = startbottom;
+	}
 }
 
 void output_mesh_bottom(int bottom, int starttop, int points)
 {
-    int a, b, c, tricount;
+	int a, b, c, tricount;
 
-    a = bottom;
-    b = starttop + 1;
-    c = starttop;
-    tricount = points;
+	a = bottom;
+	b = starttop + 1;
+	c = starttop;
+	tricount = points;
 
-    while (tricount--)
-    {
-        fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
-        b++;
-        c++;
-        if (b == starttop + points)
-            b = starttop;
-        if (c == starttop + points)
-            c = starttop;
-    }
+	while (tricount--)
+	{
+		fprintf(out, "    {{%d, %d, %d}},\n", a, b, c);
+		b++;
+		c++;
+		if (b == starttop + points)
+			b = starttop;
+		if (c == starttop + points)
+			c = starttop;
+	}
 }
 
 int main(int argc, char *argv[])
 {
-    int points, circles;
-    float x,y,z,alfa,beta,alfad,betad;
-    float steps;
-    int countp;
-    int counter = 0;
-    time_t now = time(NULL);
+	int points, circles;
+	float x, y, z, alfa, beta, alfad, betad;
+	float steps;
+	int countp;
+	int counter = 0;
+	time_t now = time(NULL);
 
-    if (argc < 4)
-    {
-        printf("Command line: %s Meridians Parallels <filename>\n", argv[0]);
-        return -1;
-    }
-    printf("Generating a sphere...\n\n======================\n\n");
+	if (argc < 4)
+	{
+		printf("Command line: %s Meridians Parallels <filename>\n", argv[0]);
+		return -1;
+	}
+	printf("Generating a sphere...\n\n======================\n\n");
 
-    points = atoi(argv[1]);
-    circles = atoi(argv[2]);
-    out = fopen(argv[3], "w");
-    if (points & 1) points++;
-    if ((circles & 1) == 0) circles++;
-    if (!out)
-    {
-        printf("Invalid file %s\n", argv[2]);
-        return -1;
-    }
-    if (points < 3)
-    {
-        printf("Too few Meridians %d!\n", points);
-        fclose(out);
-        return -1;
-    }
-    if (circles < 3)
-    {
-        printf("Too few Parallels %d!\n", circles);
-        fclose(out);
-        return -1;
-    }
-    alfad = -2.0*M_PI/(float)(points);
-    betad = 0.5f*M_PI/(float)((circles + 1)/2);
+	points = atoi(argv[1]);
+	circles = atoi(argv[2]);
+	out = fopen(argv[3], "w");
+	if (points & 1)
+		points++;
+	if ((circles & 1) == 0)
+		circles++;
+	if (!out)
+	{
+		printf("Invalid file %s\n", argv[2]);
+		return -1;
+	}
+	if (points < 3)
+	{
+		printf("Too few Meridians %d!\n", points);
+		fclose(out);
+		return -1;
+	}
+	if (circles < 3)
+	{
+		printf("Too few Parallels %d!\n", circles);
+		fclose(out);
+		return -1;
+	}
+	alfad = -2.0f * (float)M_PI / (float)(points);
+	betad = 0.5f * (float)M_PI / (float)((circles + 1) / 2);
 
-    alfa = beta = 0.0f;
-    steps = 0.0f;
+	alfa = beta = 0.0f;
+	steps = 0.0f;
 
-    fprintf(out, "/* SPHERE */\n\n/* Auto generated by %s on %s */\n\n", argv[0], ctime(&now));
-    fprintf(out, "%s\n", "struct m3d_input_point sphere[] {");
-    /* First circle not in a loop */
-    fprintf(out, "%s\n", "    /* Diameter */");
-    countp = 0;
-    y = 0.0f;
-    while (countp++ < points)
-    {
-        x = 100.0f*cos(alfa);
-        z = 100.0f*sin(alfa);
-        fprintf(out, "    {{%f, %f, %f, 1.0}}, // %d\n", x, y, z, counter++);
-        alfa += alfad;
-    }
+	fprintf(out, "/* SPHERE */\n\n/* Auto generated by %s on %s */\n\n", argv[0], ctime(&now));
+	fprintf(out, "%s\n", "struct m3d_input_point sphere[] {");
+	/* First circle not in a loop */
+	fprintf(out, "%s\n", "    /* Diameter */");
+	countp = 0;
+	y = 0.0f;
+	while (countp++ < points)
+	{
+		x = 100.0f * (float)cos(alfa);
+		z = 100.0f * (float)sin(alfa);
+		fprintf(out, "    {{%ff, %ff, %ff, 1.0f}}, // %d\n", x, y, z, counter++);
+		alfa += alfad;
+	}
 
-    steps += 0.5f;
-    beta += betad;
-    while (beta < M_PI/2)
-    {
-        alfa = steps*alfad;
-        alfa = 0.0f;
-        countp = 0;
-        fprintf(out, "%s\n", "    /* Upper circle */");
-        while (countp++ < points)
-        {
-            x = 100.0f*cos(beta)*cos(alfa);
-            y = 100.0*sin(beta);
-            z = 100.0f*cos(beta)*sin(alfa);
-            fprintf(out, "    {{%f, %f, %f, 1.0}}, //%d\n", x, y, z, counter++);
-            alfa += alfad;
-        }
-        fprintf(out, "%s\n", "    /* Lower circle */");
-        alfa = steps*alfad;
-        alfa = 0.0f;
-        countp = 0;
-        while (countp++ < points)
-        {
-            x = 100.0f*cos(beta)*cos(alfa);
-            y = -100.0*sin(beta);
-            z = 100.0f*cos(beta)*sin(alfa);
-            fprintf(out, "    {{%f, %f, %f, 1.0}}, //%d\n", x, y, z, counter++);
-            alfa += alfad;
-        }
-        steps += 0.5f;
-        beta += betad;
-    }
+	steps += 0.5f;
+	beta += betad;
+	while (beta < M_PI / 2)
+	{
+		alfa = steps * alfad;
+		alfa = 0.0f;
+		countp = 0;
+		fprintf(out, "%s\n", "    /* Upper circle */");
+		while (countp++ < points)
+		{
+			x = 100.0f * (float)cos(beta) * (float)cos(alfa);
+			y = 100.0f * (float)sin(beta);
+			z = 100.0f * (float)cos(beta) * (float)sin(alfa);
+			fprintf(out, "    {{%ff, %ff, %ff, 1.0f}}, //%d\n", x, y, z, counter++);
+			alfa += alfad;
+		}
+		fprintf(out, "%s\n", "    /* Lower circle */");
+		alfa = steps * alfad;
+		alfa = 0.0f;
+		countp = 0;
+		while (countp++ < points)
+		{
+			x = 100.0f * (float)cos(beta) * (float)cos(alfa);
+			y = -100.0f * (float)sin(beta);
+			z = 100.0f * (float)cos(beta) * (float)sin(alfa);
+			fprintf(out, "    {{%ff, %ff, %ff, 1.0f}}, //%d\n", x, y, z, counter++);
+			alfa += alfad;
+		}
+		steps += 0.5f;
+		beta += betad;
+	}
 
-    fprintf(out, "%s\n", "    /* Top */");
-    fprintf(out, "    {{0.0, 100.0, 0.0, 1.0}}, //%d\n", counter++);
-    fprintf(out, "%s\n", "    /* Bottom */");
-    fprintf(out, "    {{0.0, -100.0, 0.0, 1.0}} //%d\n", counter++);
-    fprintf(out, "%s\n", "};\n");
+	fprintf(out, "%s\n", "    /* Top */");
+	fprintf(out, "    {{0.0f, 100.0f, 0.0f, 1.0f}}, //%d\n", counter++);
+	fprintf(out, "%s\n", "    /* Bottom */");
+	fprintf(out, "    {{0.0f, -100.0f, 0.0f, 1.0f}} //%d\n", counter++);
+	fprintf(out, "%s\n", "};\n");
 
-/*
- *                      
- *                     5
- *                     3  
- *                     1
- *                     0
- *                     2
- *                     4
- *                     6
- * 
- * 
- */ 
+	/*
+	 *
+	 *                     5
+	 *                     3
+	 *                     1
+	 *                     0
+	 *                     2
+	 *                     4
+	 *                     6
+	 *
+	 *
+	 */
 
-    fprintf(out, "%s\n", "struct m3d_input_trimesh spheremesh[] = {");
-    countp = 0;
-    fprintf(out, "%s\n", "    /* Circle */");
-    output_mesh_up(countp, countp + points, points);
-    fprintf(out, "%s\n", "    /* Circle */");
-    output_mesh_down(countp + 2*points, countp, points);
-    countp += points;
-    counter -= 3*points;
-    while (counter != 2)
-    {
-        fprintf(out, "%s\n", "    /* Circle */");
-        output_mesh_up(countp, countp + 2*points, points);
-        fprintf(out, "%s\n", "    /* Circle */");
-        output_mesh_down(countp + 3*points, countp + points, points);
-        countp += 2*points;
-        counter -= 2*points;
-    }
+	fprintf(out, "%s\n", "struct m3d_input_trimesh spheremesh[] = {");
+	countp = 0;
+	fprintf(out, "%s\n", "    /* Circle */");
+	output_mesh_up(countp, countp + points, points);
+	fprintf(out, "%s\n", "    /* Circle */");
+	output_mesh_down(countp + 2 * points, countp, points);
+	countp += points;
+	counter -= 3 * points;
+	while (counter != 2)
+	{
+		fprintf(out, "%s\n", "    /* Circle */");
+		output_mesh_up(countp, countp + 2 * points, points);
+		fprintf(out, "%s\n", "    /* Circle */");
+		output_mesh_down(countp + 3 * points, countp + points, points);
+		countp += 2 * points;
+		counter -= 2 * points;
+	}
 
-    fprintf(out, "%s\n", "    /* Top */");
-    output_mesh_top(countp, countp + 2*points, points);
-    fprintf(out, "%s\n", "    /* Bottom */");
-    output_mesh_bottom(countp + 2*points + 1, countp + points, points);
-    fprintf(out, "%s\n", "};\n");
-    fclose(out);
+	fprintf(out, "%s\n", "    /* Top */");
+	output_mesh_top(countp, countp + 2 * points, points);
+	fprintf(out, "%s\n", "    /* Bottom */");
+	output_mesh_bottom(countp + 2 * points + 1, countp + points, points);
+	fprintf(out, "%s\n", "};\n");
+	fclose(out);
 
-    return 0;
+	return 0;
 }
