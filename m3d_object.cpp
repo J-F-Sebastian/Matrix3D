3895925f91ba2247dafebf4e783656d00a5fd8b6
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2024-05-31 20:39:48 +0200

    m3d_object: run indentation

--- m3d_object.cpp
+++ m3d_object.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 void m3d_triangle::rotate(m3d_matrix &rotation)
 {
-    rotation.rotate(normal, normal);
+	rotation.rotate(normal, normal);
 }
 
 /******************************/
@@ -22,81 +22,81 @@ void m3d_triangle::rotate(m3d_matrix &rotation)
 /******************************/
 
 int m3d_object::create(struct m3d_input_point *vertices,
-                       const uint32_t vertnum,
-                       struct m3d_input_trimesh *mesh,
-                       const uint32_t meshnum)
+		       const uint32_t vertnum,
+		       struct m3d_input_trimesh *mesh,
+		       const uint32_t meshnum)
 {
-    vector<m3d_vertex>::iterator it;
-    vector<m3d_triangle>::iterator it2;
-    m3d_vector a, b;
+	vector<m3d_vertex>::iterator it;
+	vector<m3d_triangle>::iterator it2;
+	m3d_vector a, b;
 
-    if (vertices && vertnum && (vertnum < M3D_MAX_VERTICES) && this->vertices.empty())
-    {
-        try
-        {
-            this->vertices.resize(vertnum);
-        }
-        catch (const bad_alloc &e)
-        {
-            cerr << "Out of memory " << e.what() << endl;
-            return (ENOMEM);
-        }
-        for (it = this->vertices.begin(); it != this->vertices.end(); it++)
-        {
-            it->position = vertices->vector;
-            vertices++;
-        }
-        vertex_visible.reset();
-    }
-    else
-    {
-        return (EINVAL);
-    }
+	if (vertices && vertnum && (vertnum < M3D_MAX_VERTICES) && this->vertices.empty())
+	{
+		try
+		{
+			this->vertices.resize(vertnum);
+		}
+		catch (const bad_alloc &e)
+		{
+			cerr << "Out of memory " << e.what() << endl;
+			return (ENOMEM);
+		}
+		for (it = this->vertices.begin(); it != this->vertices.end(); it++)
+		{
+			it->position = vertices->vector;
+			vertices++;
+		}
+		vertex_visible.reset();
+	}
+	else
+	{
+		return (EINVAL);
+	}
 
-    if (mesh && meshnum && (meshnum < M3D_MAX_TRIANGLES) && this->mesh.empty())
-    {
-        try
-        {
-            this->mesh.resize(meshnum);
-        }
-        catch (const bad_alloc &e)
-        {
-            cerr << "Out of memory " << e.what() << endl;
-            return (ENOMEM);
-        }
+	if (mesh && meshnum && (meshnum < M3D_MAX_TRIANGLES) && this->mesh.empty())
+	{
+		try
+		{
+			this->mesh.resize(meshnum);
+		}
+		catch (const bad_alloc &e)
+		{
+			cerr << "Out of memory " << e.what() << endl;
+			return (ENOMEM);
+		}
 
-        for (it2 = this->mesh.begin(); it2 != this->mesh.end(); it2++, mesh++)
-        {
-            it2->index[0] = mesh->index[0];
-            it2->index[1] = mesh->index[1];
-            it2->index[2] = mesh->index[2];
-            /*
-            * compute the surface normal.
-            * vertices will be used this way:
-            *    0 -> 1   vector 1
-            *    0 -> 2   vector 2
-            *
-            * normal is vector1 X vector2
-            */
-            a = this->vertices.at(mesh->index[1]).position;
-            a.subtract(this->vertices.at(mesh->index[0]).position);
-            b = this->vertices.at(mesh->index[2]).position;
-            b.subtract(this->vertices.at(mesh->index[0]).position);
-            a.cross_product(b);
-            a.normalize();
-            it2->normal = a;
-        }
-        triangle_visible.reset();
-    }
-    else
-    {
-        return (EINVAL);
-    }
+		for (it2 = this->mesh.begin(); it2 != this->mesh.end(); it2++, mesh++)
+		{
+			it2->index[0] = mesh->index[0];
+			it2->index[1] = mesh->index[1];
+			it2->index[2] = mesh->index[2];
+			/*
+			 * compute the surface normal.
+			 * vertices will be used this way:
+			 *    0 -> 1   vector 1
+			 *    0 -> 2   vector 2
+			 *
+			 * normal is vector1 X vector2
+			 */
+			a = this->vertices.at(mesh->index[1]).position;
+			a.subtract(this->vertices.at(mesh->index[0]).position);
+			b = this->vertices.at(mesh->index[2]).position;
+			b.subtract(this->vertices.at(mesh->index[0]).position);
+			a.cross_product(b);
+			a.normalize();
+			it2->normal = a;
+		}
+		triangle_visible.reset();
+	}
+	else
+	{
+		return (EINVAL);
+	}
 
-    compute_center();
-    // DIRECTION
+	compute_center();
+	// DIRECTION
 
-    return (0);
+	return (0);
 }
 
 /*
@@ -104,9 +104,9 @@ int m3d_object::create(struct m3d_input_point *vertices,
  */
 void m3d_object::roll(float angle)
 {
-    m3d_matrix temp = m3d_matrix_roll(angle);
-    update_object(temp);
-    visibility_uptodate = false;
+	m3d_matrix temp = m3d_matrix_roll(angle);
+	update_object(temp);
+	visibility_uptodate = false;
 }
 
 /*
@@ -114,9 +114,9 @@ void m3d_object::roll(float angle)
  */
 void m3d_object::yaw(float angle)
 {
-    m3d_matrix temp = m3d_matrix_yaw(angle);
-    update_object(temp);
-    visibility_uptodate = false;
+	m3d_matrix temp = m3d_matrix_yaw(angle);
+	update_object(temp);
+	visibility_uptodate = false;
 }
 
 /*
@@ -124,129 +124,129 @@ void m3d_object::yaw(float angle)
  */
 void m3d_object::pitch(float angle)
 {
-    m3d_matrix temp = m3d_matrix_pitch(angle);
-    update_object(temp);
-    visibility_uptodate = false;
+	m3d_matrix temp = m3d_matrix_pitch(angle);
+	update_object(temp);
+	visibility_uptodate = false;
 }
 
 // move the object to a new location
 void m3d_object::move(const m3d_vector &newposition)
 {
-    center.add(newposition);
-    visibility_uptodate = false;
+	center.add(newposition);
+	visibility_uptodate = false;
 }
 
 void m3d_object::print()
 {
 #ifdef NDEBUG
-    vector<m3d_vertex>::iterator it;
-    vector<m3d_triangle>::iterator it2;
-    ostringstream tmp;
-    unsigned i;
+	vector<m3d_vertex>::iterator it;
+	vector<m3d_triangle>::iterator it2;
+	ostringstream tmp;
+	unsigned i;
 
-    cout << "Object" << endl;
-    for (it = vertices.begin(), i = 0; it != vertices.end(); it++, i++)
-    {
-        if (vertex_visible[i])
-        {
-            cout << " V, ";
-        }
-        else
-        {
-            cout << "    ";
-        }
-        it->print();
-    }
-    cout << "Center ---> ";
-    center.print();
-    cout << "Triangle Mesh" << endl;
-    for (it2 = mesh.begin(), i = 0; it2 != mesh.end(); it2++, i++)
-    {
-        tmp << "(" << it2->index[0] << "," << it2->index[1] << "," << it2->index[2] << ") ,";
-        if (triangle_visible[i])
-        {
-            tmp << " V,";
-        }
-        tmp << " Normal ---> ";
-        cout << tmp.str();
-        tmp.str("");
-        it2->normal.print();
-    }
-    cout << endl;
+	cout << "Object" << endl;
+	for (it = vertices.begin(), i = 0; it != vertices.end(); it++, i++)
+	{
+		if (vertex_visible[i])
+		{
+			cout << " V, ";
+		}
+		else
+		{
+			cout << "    ";
+		}
+		it->print();
+	}
+	cout << "Center ---> ";
+	center.print();
+	cout << "Triangle Mesh" << endl;
+	for (it2 = mesh.begin(), i = 0; it2 != mesh.end(); it2++, i++)
+	{
+		tmp << "(" << it2->index[0] << "," << it2->index[1] << "," << it2->index[2] << ") ,";
+		if (triangle_visible[i])
+		{
+			tmp << " V,";
+		}
+		tmp << " Normal ---> ";
+		cout << tmp.str();
+		tmp.str("");
+		it2->normal.print();
+	}
+	cout << endl;
 #endif
 }
 
 void m3d_object::compute_center()
 {
-    float part = 1.0f / (float)vertices.size();
-    vector<m3d_vertex>::iterator it;
+	float part = 1.0f / (float)vertices.size();
+	vector<m3d_vertex>::iterator it;
 
-    for (it = vertices.begin(); it != vertices.end(); it++)
-    {
-        center.add(it->position);
-    }
+	for (it = vertices.begin(); it != vertices.end(); it++)
+	{
+		center.add(it->position);
+	}
 
-    center.myvector[X_C] *= part;
-    center.myvector[Y_C] *= part;
-    center.myvector[Z_C] *= part;
-    center.myvector[T_C] = 1.0f;
+	center.myvector[X_C] *= part;
+	center.myvector[Y_C] *= part;
+	center.myvector[Z_C] *= part;
+	center.myvector[T_C] = 1.0f;
 }
 
 void m3d_object::update_object(m3d_matrix &transform)
 {
-    vector<m3d_vertex>::iterator it = vertices.begin();
-    vector<m3d_triangle>::iterator it2 = mesh.begin();
+	vector<m3d_vertex>::iterator it = vertices.begin();
+	vector<m3d_triangle>::iterator it2 = mesh.begin();
 
-    while (it != vertices.end())
-    {
-        transform.rotate(it->position, it->position);
-        transform.rotate(it->normal, it->normal);
-        ++it;
-    }
+	while (it != vertices.end())
+	{
+		transform.rotate(it->position, it->position);
+		transform.rotate(it->normal, it->normal);
+		++it;
+	}
 
-    /* update triangle surfaces' normals */
-    while (it2 != mesh.end())
-    {
-        it2->rotate(transform);
-        ++it2;
-    }
+	/* update triangle surfaces' normals */
+	while (it2 != mesh.end())
+	{
+		it2->rotate(transform);
+		++it2;
+	}
 
-    /* update object's direction */
-    /* MISSING */
+	/* update object's direction */
+	/* MISSING */
 }
 
 void m3d_object::compute_visibility(m3d_camera &viewpoint)
 {
-    vector<m3d_triangle>::iterator it;
-    m3d_point temp;
-    unsigned i;
+	vector<m3d_triangle>::iterator it;
+	m3d_point temp;
+	unsigned i;
 
-    if (visibility_uptodate)
-    {
-        return;
-    }
+	if (visibility_uptodate)
+	{
+		return;
+	}
 
-    triangle_visible.reset();
-    vertex_visible.reset();
-    /*
-     * Compute visibility by adding center to the first triangle vertex,
-     * then subtracting the viewpoint, then invoking is_visible.
-     */
-    for (it = mesh.begin(), i = 0; it != mesh.end(); it++, i++)
-    {
-        temp = vertices.at(it->index[0]).position;
-        // temp is now in world coordinates
-        temp.add(center);
-        triangle_visible[i] = viewpoint.is_visible(temp, it->normal);
-        if (triangle_visible[i])
-        {
-            vertex_visible[it->index[0]] = true;
-            vertex_visible[it->index[1]] = true;
-            vertex_visible[it->index[2]] = true;
-        }
-    }
+	triangle_visible.reset();
+	vertex_visible.reset();
+	/*
+	 * Compute visibility by adding center to the first triangle vertex,
+	 * then subtracting the viewpoint, then invoking is_visible.
+	 */
+	for (it = mesh.begin(), i = 0; it != mesh.end(); it++, i++)
+	{
+		temp = vertices.at(it->index[0]).position;
+		// temp is now in world coordinates
+		temp.add(center);
+		triangle_visible[i] = viewpoint.is_visible(temp, it->normal);
+		if (triangle_visible[i])
+		{
+			vertex_visible[it->index[0]] = true;
+			vertex_visible[it->index[1]] = true;
+			vertex_visible[it->index[2]] = true;
+		}
+	}
 
-    visibility_uptodate = true;
+	visibility_uptodate = true;
 }
 
 /*************************************/
@@ -254,51 +254,51 @@ void m3d_object::compute_visibility(m3d_camera &viewpoint)
 /*************************************/
 
 int m3d_render_object::create(struct m3d_input_point *vertices,
-                              const uint32_t vertnum,
-                              struct m3d_input_trimesh *mesh,
-                              const uint32_t meshnum,
-                              m3d_color &color)
+			      const uint32_t vertnum,
+			      struct m3d_input_trimesh *mesh,
+			      const uint32_t meshnum,
+			      m3d_color &color)
 {
-    vector<m3d_triangle>::iterator it;
-    vector<m3d_vertex>::iterator it2;
-    int retcode;
+	vector<m3d_triangle>::iterator it;
+	vector<m3d_vertex>::iterator it2;
+	int retcode;
 
-    retcode = m3d_object::create(vertices, vertnum, mesh, meshnum);
+	retcode = m3d_object::create(vertices, vertnum, mesh, meshnum);
 
-    if (retcode)
-    {
-        return (retcode);
-    }
+	if (retcode)
+	{
+		return (retcode);
+	}
 
-    // Set vertices normals by summing triangles' normals to every
-    // vertices.
-    // Triangles' normals are orthogonal to the surface, Vertices' normals
-    // are the normalized sum of surfaces' normals
-    //
-    for (it = this->mesh.begin(); it != this->mesh.end(); it++)
-    {
-        this->vertices.at(it->index[0]).normal.add(it->normal);
-        this->vertices.at(it->index[1]).normal.add(it->normal);
-        this->vertices.at(it->index[2]).normal.add(it->normal);
-    }
+	// Set vertices normals by summing triangles' normals to every
+	// vertices.
+	// Triangles' normals are orthogonal to the surface, Vertices' normals
+	// are the normalized sum of surfaces' normals
+	//
+	for (it = this->mesh.begin(); it != this->mesh.end(); it++)
+	{
+		this->vertices.at(it->index[0]).normal.add(it->normal);
+		this->vertices.at(it->index[1]).normal.add(it->normal);
+		this->vertices.at(it->index[2]).normal.add(it->normal);
+	}
 
-    // Normalize
-    for (it2 = this->vertices.begin(); it2 != this->vertices.end(); it2++)
-    {
-        it2->normal.print();
-        it2->normal.normalize();
-    }
+	// Normalize
+	for (it2 = this->vertices.begin(); it2 != this->vertices.end(); it2++)
+	{
+		it2->normal.print();
+		it2->normal.normalize();
+	}
 
-    this->color = color;
-    return (0);
+	this->color = color;
+	return (0);
 };
 
 void m3d_render_object::print()
 {
 #ifdef NDEBUG
-    cout << "Render Object" << endl;
-    m3d_object::print();
-    color.print();
-    cout << endl;
+	cout << "Render Object" << endl;
+	m3d_object::print();
+	color.print();
+	cout << endl;
 #endif
 }
--- m3d_object.hh
+++ m3d_object.hh
@@ -9,7 +9,7 @@
 #include "m3d_camera.hh"
 
 #define M3D_MAX_VERTICES (1024)
-#define M3D_MAX_TRIANGLES (1024*4)
+#define M3D_MAX_TRIANGLES (1024 * 4)
 
 /*
  * This data structure is used to create a mesh of points,
@@ -25,151 +25,150 @@
  *
  *  m3d_input_trimesh triangle_triangle = {{0,1,2}};
  */
-struct m3d_input_trimesh {
-    uint32_t index[3];
+struct m3d_input_trimesh
+{
+	uint32_t index[3];
 };
 
 class m3d_triangle
 {
 public:
-    m3d_triangle() : normal()
-    {
-        index[0] = index[1] = index[2] = 0;
-    }
+	m3d_triangle() : normal()
+	{
+		index[0] = index[1] = index[2] = 0;
+	}
 
-    m3d_triangle(const m3d_triangle& other) : normal(other.normal)
-    {
-        index[0] = other.index[0];
-        index[1] = other.index[1];
-        index[2] = other.index[2];
-    }
+	m3d_triangle(const m3d_triangle &other) : normal(other.normal)
+	{
+		index[0] = other.index[0];
+		index[1] = other.index[1];
+		index[2] = other.index[2];
+	}
 
-    ~m3d_triangle() {};
+	~m3d_triangle(){};
 
-    void rotate(m3d_matrix &rotation);
+	void rotate(m3d_matrix &rotation);
 
-    uint32_t index[3];
-    m3d_vector normal;
+	uint32_t index[3];
+	m3d_vector normal;
 };
 
 class m3d_object
 {
 public:
-    /** Default constructor */
-    m3d_object() :
-        vertices(),
-        mesh(),
-        vertex_visible(),
-        triangle_visible(),
-        direction(),
-        center(),
-        z_sorting(0.0f),
-        visibility_uptodate(false) {};
-    /** Default destructor */
-    ~m3d_object() {};
-    /** Copy constructor
-     *  \param other Object to copy from
-     */
-    m3d_object(const m3d_object& other) :
-        vertices(other.vertices),
-        mesh(other.mesh),
-        vertex_visible(other.vertex_visible),
-        triangle_visible(other.triangle_visible),
-        direction(other.direction),
-        center(other.center),
-        z_sorting(0.0f),
-        visibility_uptodate(false) {};
+	/** Default constructor */
+	m3d_object() : vertices(),
+		       mesh(),
+		       vertex_visible(),
+		       triangle_visible(),
+		       direction(),
+		       center(),
+		       z_sorting(0.0f),
+		       visibility_uptodate(false){};
+	/** Default destructor */
+	~m3d_object(){};
+	/** Copy constructor
+	 *  \param other Object to copy from
+	 */
+	m3d_object(const m3d_object &other) : vertices(other.vertices),
+					      mesh(other.mesh),
+					      vertex_visible(other.vertex_visible),
+					      triangle_visible(other.triangle_visible),
+					      direction(other.direction),
+					      center(other.center),
+					      z_sorting(0.0f),
+					      visibility_uptodate(false){};
 
-    // Set vertices, mesh, their normals and cleanup bitmaps
-    int create(struct m3d_input_point *vertices,
-               const uint32_t vertnum,
-               struct m3d_input_trimesh *mesh,
-               const uint32_t meshnum);
+	// Set vertices, mesh, their normals and cleanup bitmaps
+	int create(struct m3d_input_point *vertices,
+		   const uint32_t vertnum,
+		   struct m3d_input_trimesh *mesh,
+		   const uint32_t meshnum);
 
-    /*
-     * perform rolling of the object, rotating around z angle in degrees
-     */
-    void roll(float angle);
+	/*
+	 * perform rolling of the object, rotating around z angle in degrees
+	 */
+	void roll(float angle);
 
-    /*
-     * perform yawing of the object, rotating around y angle in degrees
-     */
-    void yaw(float angle);
+	/*
+	 * perform yawing of the object, rotating around y angle in degrees
+	 */
+	void yaw(float angle);
 
-    /*
-     * perform pitching of the object, rotating around x angle in degrees
-     */
-    void pitch(float angle);
+	/*
+	 * perform pitching of the object, rotating around x angle in degrees
+	 */
+	void pitch(float angle);
 
-    // move the object to a new location along the passed-in vector
-    void move(const m3d_vector &newposition);
+	// move the object to a new location along the passed-in vector
+	void move(const m3d_vector &newposition);
 
-    // Compute triangles visibility
-    void compute_visibility(m3d_camera &viewpoint);
+	// Compute triangles visibility
+	void compute_visibility(m3d_camera &viewpoint);
 
-    void print(void);
+	void print(void);
 
-    // vertices are always in object coordinates, referring to a center in [0,0,0]
-    // vertices stores vectors to vertices and their normals
-    std::vector<m3d_vertex> vertices;
-    // mesh stores triangles and their normals
-    std::vector<m3d_triangle> mesh;
-    // visible vertices, visible triangles
-    std::bitset<M3D_MAX_VERTICES> vertex_visible;
-    std::bitset<M3D_MAX_TRIANGLES> triangle_visible;
-    // Object direction, used for orientation and texturing
-    m3d_vector direction;
-    // center is always in world coordinates
-    m3d_point center;
-    // This is a convenience for sorting
-    float z_sorting;
+	// vertices are always in object coordinates, referring to a center in [0,0,0]
+	// vertices stores vectors to vertices and their normals
+	std::vector<m3d_vertex> vertices;
+	// mesh stores triangles and their normals
+	std::vector<m3d_triangle> mesh;
+	// visible vertices, visible triangles
+	std::bitset<M3D_MAX_VERTICES> vertex_visible;
+	std::bitset<M3D_MAX_TRIANGLES> triangle_visible;
+	// Object direction, used for orientation and texturing
+	m3d_vector direction;
+	// center is always in world coordinates
+	m3d_point center;
+	// This is a convenience for sorting
+	float z_sorting;
 
 protected:
-    void update_object(m3d_matrix& transform);
+	void update_object(m3d_matrix &transform);
 
 private:
-    /** Compute the object center */
-    void compute_center(void);
-    bool visibility_uptodate;
+	/** Compute the object center */
+	void compute_center(void);
+	bool visibility_uptodate;
 };
 
 class m3d_render_object : public m3d_object
 {
 public:
-    enum {
-        OBJ_VISIBLE          = 1 << 0,
-        OBJ_CHANGED          = 1 << 7
-    };
+	enum
+	{
+		OBJ_VISIBLE = 1 << 0,
+		OBJ_CHANGED = 1 << 7
+	};
 
-    /** Default constructor */
-    m3d_render_object() : m3d_object(), color() {};
-    /** Default destructor */
-    virtual ~m3d_render_object() {};
-    /** Copy constructor
-        *  \param other Object to copy from
-        */
-    m3d_render_object(const m3d_render_object& other) :
-        m3d_object(other), color(other.color) {};
+	/** Default constructor */
+	m3d_render_object() : m3d_object(), color(){};
+	/** Default destructor */
+	virtual ~m3d_render_object(){};
+	/** Copy constructor
+	 *  \param other Object to copy from
+	 */
+	m3d_render_object(const m3d_render_object &other) : m3d_object(other), color(other.color){};
 
-    // creates an object starting from a m3d_trimesh array.
-    // vertices points to an array of m3d_point structs, describing the geometry of all
-    // vertexes (points) of the object.
-    // mesh must point to an array of m3d_input_trimesh structs, describing the geometry of
-    // the object as a list of triangles, whose vertexes are stored in vertices.
-    // Direction is a vector starting at (0,0,0,0) and pointed towards face number 0.
-    // vertnum is the number of items in vertices.
-    // meshnum is the number of items in mesh.
-    int create(struct m3d_input_point *vertices,
-               const uint32_t vertnum,
-               struct m3d_input_trimesh *mesh,
-               const uint32_t meshnum,
-               m3d_color &color);
+	// creates an object starting from a m3d_trimesh array.
+	// vertices points to an array of m3d_point structs, describing the geometry of all
+	// vertexes (points) of the object.
+	// mesh must point to an array of m3d_input_trimesh structs, describing the geometry of
+	// the object as a list of triangles, whose vertexes are stored in vertices.
+	// Direction is a vector starting at (0,0,0,0) and pointed towards face number 0.
+	// vertnum is the number of items in vertices.
+	// meshnum is the number of items in mesh.
+	int create(struct m3d_input_point *vertices,
+		   const uint32_t vertnum,
+		   struct m3d_input_trimesh *mesh,
+		   const uint32_t meshnum,
+		   m3d_color &color);
 
-    void print(void);
+	void print(void);
 
-    m3d_color color;
+	m3d_color color;
 
-    unsigned flags;
+	unsigned flags;
 };
 
 #endif // M3D_OBJECT_H
