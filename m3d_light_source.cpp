5e379a26b93b72d88915ce972bd05fe0d43a4ab5
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2024-05-31 20:40:05 +0200

    m3d_light_source: run indentation

--- m3d_light_source.cpp
+++ m3d_light_source.cpp
@@ -6,74 +6,74 @@ using namespace std;
 
 static inline float m3d_min(float a, float b)
 {
-    return (a < b) ? a : b;
+	return (a < b) ? a : b;
 }
 
 void m3d_ambient_light::print(void)
 {
 #ifdef NDEBUG
-    cout << "Ambient light "
-         << "Color " << color.getColor() << "Intensity " << sintensity << endl;
+	cout << "Ambient light "
+	     << "Color " << color.getColor() << "Intensity " << sintensity << endl;
 #endif
 }
 
-//Intensity of the light color is computed with the formula
+// Intensity of the light color is computed with the formula
 //
-// C' = 1/(Kc + Kl*d + Kq*d^2) * C
+//  C' = 1/(Kc + Kl*d + Kq*d^2) * C
 //
-// So the light color is modulated by a quadratic formula for its intensity.
-// Light intensity depends on distance between the light source and
-// the object position
+//  So the light color is modulated by a quadratic formula for its intensity.
+//  Light intensity depends on distance between the light source and
+//  the object position
 float m3d_point_light_source::get_intensity(const m3d_point &objpos, const m3d_point & /*viewer*/)
 {
-    // compute vector from position to light source
-    m3d_vector L(objpos);
-    L.subtract(position);
-    float d = L.module();
-    //cout << "Point light intensity " << sintensity/(Kc + Kl*d + Kq*d*d) << endl;
-    return (sintensity / (Kc + Kl * d + Kq * d * d));
+	// compute vector from position to light source
+	m3d_vector L(objpos);
+	L.subtract(position);
+	float d = L.module();
+	// cout << "Point light intensity " << sintensity/(Kc + Kl*d + Kq*d*d) << endl;
+	return (sintensity / (Kc + Kl * d + Kq * d * d));
 }
 
 void m3d_point_light_source::print()
 {
 #ifdef NDEBUG
-    cout.setf(ios_base::showpoint);
-    cout << "Light source" << endl
-         << "Kc " << Kc << ", Kl " << Kl << ", Kq " << Kq << endl;
-    position.print();
+	cout.setf(ios_base::showpoint);
+	cout << "Light source" << endl
+	     << "Kc " << Kc << ", Kl " << Kl << ", Kq " << Kq << endl;
+	position.print();
 #endif
 }
 
-//Intensity of the light color is computed with the formula
+// Intensity of the light color is computed with the formula
 //
-// C' = 1/(Kc + Kl*d + Kq*d^2) * C
+//  C' = 1/(Kc + Kl*d + Kq*d^2) * C
 //
-// So the light color is modulated by a quadratic formula for its intensity.
-// Light intensity depends on distance between the light source and
-// the object position
+//  So the light color is modulated by a quadratic formula for its intensity.
+//  Light intensity depends on distance between the light source and
+//  the object position
 float m3d_spot_light_source::get_intensity(const m3d_point &objpos, const m3d_point &viewer)
 {
-    // compute vector from position to light source
-    m3d_vector L(objpos);
-    m3d_vector temppos(get_position());
-    L.subtract(temppos);
-    // compute RdotL
-    float RdotL = m3d_min(L.dot_product(direction), 0.0f);
-    if (RdotL != 0.0f)
-    {
-        RdotL *= m3d_point_light_source::get_intensity(objpos, viewer);
-    }
+	// compute vector from position to light source
+	m3d_vector L(objpos);
+	m3d_vector temppos(get_position());
+	L.subtract(temppos);
+	// compute RdotL
+	float RdotL = m3d_min(L.dot_product(direction), 0.0f);
+	if (RdotL != 0.0f)
+	{
+		RdotL *= m3d_point_light_source::get_intensity(objpos, viewer);
+	}
 
-    return RdotL;
+	return RdotL;
 }
 
 void m3d_spot_light_source::print()
 {
 #ifdef NDEBUG
-    cout.setf(ios_base::showpoint);
-    cout << "Light source" << endl
-         << "Kc " << Kc << ", Kl " << Kl << ", Kq " << Kq << endl;
-    position.print();
-    direction.print();
+	cout.setf(ios_base::showpoint);
+	cout << "Light source" << endl
+	     << "Kc " << Kc << ", Kl " << Kl << ", Kq " << Kq << endl;
+	position.print();
+	direction.print();
 #endif
 }
--- m3d_light_source.hh
+++ m3d_light_source.hh
@@ -11,127 +11,127 @@
 class m3d_light_source
 {
 public:
-    /** Default constructor */
-    m3d_light_source() : color(), sintensity(0.0f){};
+	/** Default constructor */
+	m3d_light_source() : color(), sintensity(0.0f){};
 
-    /** Default destructor */
-    virtual ~m3d_light_source(){};
-    /** Copy constructor
-     *  \param other Object to copy from
-     */
-    m3d_light_source(const m3d_light_source &other) : color(other.color), sintensity(other.sintensity){};
+	/** Default destructor */
+	virtual ~m3d_light_source(){};
+	/** Copy constructor
+	 *  \param other Object to copy from
+	 */
+	m3d_light_source(const m3d_light_source &other) : color(other.color), sintensity(other.sintensity){};
 
-    m3d_light_source(const m3d_color &color, float src_intensity) : color(color), sintensity(src_intensity){};
+	m3d_light_source(const m3d_color &color, float src_intensity) : color(color), sintensity(src_intensity){};
 
-    float get_intensity(const m3d_point & /*objpos*/, const m3d_point & /*viewer*/) { return sintensity; }
+	float get_intensity(const m3d_point & /*objpos*/, const m3d_point & /*viewer*/) { return sintensity; }
 
-    virtual m3d_color get_color(void) = 0;
-    void set_color(const m3d_color &color) { this->color = color; }
-    void set_src_intensity(float intensity) { sintensity = (std::signbit(intensity)) ? 0.0f : intensity; };
+	virtual m3d_color get_color(void) = 0;
+	void set_color(const m3d_color &color) { this->color = color; }
+	void set_src_intensity(float intensity) { sintensity = (std::signbit(intensity)) ? 0.0f : intensity; };
 
-    virtual void print(void) = 0;
+	virtual void print(void) = 0;
 
 protected:
-    m3d_color color;
-    float sintensity;
+	m3d_color color;
+	float sintensity;
 };
 
 // Ambient light
 class m3d_ambient_light : public m3d_light_source
 {
 public:
-    m3d_ambient_light() : m3d_light_source(){};
-    virtual ~m3d_ambient_light(){};
+	m3d_ambient_light() : m3d_light_source(){};
+	virtual ~m3d_ambient_light(){};
 
-    m3d_ambient_light(const m3d_ambient_light &other) : m3d_light_source(other){};
+	m3d_ambient_light(const m3d_ambient_light &other) : m3d_light_source(other){};
 
-    m3d_ambient_light(const m3d_color &color,
-                      const float src_intensity) : m3d_light_source(color, src_intensity) {}
+	m3d_ambient_light(const m3d_color &color,
+			  const float src_intensity) : m3d_light_source(color, src_intensity) {}
 
-    //Ambient light is constant everywhere, objects and viewer position are not involved
-    //get_intensity is inherited
-    virtual m3d_color get_color(void) { return color; }
+	// Ambient light is constant everywhere, objects and viewer position are not involved
+	// get_intensity is inherited
+	virtual m3d_color get_color(void) { return color; }
 
-    virtual void print(void);
+	virtual void print(void);
 };
 
 // point light source
 class m3d_point_light_source : public m3d_light_source
 {
 public:
-    /** Default constructor */
-    m3d_point_light_source() : m3d_light_source(), position(), Kc(0.0f), Kl(0.0f), Kq(0.0f) {}
+	/** Default constructor */
+	m3d_point_light_source() : m3d_light_source(), position(), Kc(0.0f), Kl(0.0f), Kq(0.0f) {}
 
-    /** Default destructor */
-    virtual ~m3d_point_light_source(){};
-    /** Copy constructor
-     *  \param other Object to copy from
-     */
-    m3d_point_light_source(const m3d_point_light_source &other) : m3d_light_source(other),
-                                                                  position(other.position),
-                                                                  Kc(other.Kc),
-                                                                  Kl(other.Kl),
-                                                                  Kq(other.Kq) {}
+	/** Default destructor */
+	virtual ~m3d_point_light_source(){};
+	/** Copy constructor
+	 *  \param other Object to copy from
+	 */
+	m3d_point_light_source(const m3d_point_light_source &other) : m3d_light_source(other),
+								      position(other.position),
+								      Kc(other.Kc),
+								      Kl(other.Kl),
+								      Kq(other.Kq) {}
 
-    m3d_point_light_source(const struct m3d_input_point &position,
-                           const m3d_color &color,
-                           const float Kc,
-                           const float Kl,
-                           const float Kq,
-                           const float intensity) : m3d_light_source(color, intensity),
-                                                    position(position),
-                                                    Kc(Kc),
-                                                    Kl(Kl),
-                                                    Kq(Kq) {}
+	m3d_point_light_source(const struct m3d_input_point &position,
+			       const m3d_color &color,
+			       const float Kc,
+			       const float Kl,
+			       const float Kq,
+			       const float intensity) : m3d_light_source(color, intensity),
+							position(position),
+							Kc(Kc),
+							Kl(Kl),
+							Kq(Kq) {}
 
-    inline const m3d_point &get_position(void) { return position; }
+	inline const m3d_point &get_position(void) { return position; }
 
-    float get_intensity(const m3d_point &objpos, const m3d_point &viewer);
+	float get_intensity(const m3d_point &objpos, const m3d_point &viewer);
 
-    virtual m3d_color get_color(void) { return color; }
+	virtual m3d_color get_color(void) { return color; }
 
-    virtual void print(void);
+	virtual void print(void);
 
 protected:
-    m3d_point position;
-    //Constant, Linear and Quadratic attenuation constants
-    float Kc, Kl, Kq;
+	m3d_point position;
+	// Constant, Linear and Quadratic attenuation constants
+	float Kc, Kl, Kq;
 };
 
 // spot light source
 class m3d_spot_light_source : public m3d_point_light_source
 {
 public:
-    /** Default constructor */
-    m3d_spot_light_source() : m3d_point_light_source() {}
+	/** Default constructor */
+	m3d_spot_light_source() : m3d_point_light_source() {}
 
-    /** Default destructor */
-    virtual ~m3d_spot_light_source(){};
-    /** Copy constructor
-     *  \param other Object to copy from
-     */
-    m3d_spot_light_source(const m3d_spot_light_source &other) : m3d_point_light_source(other) {}
+	/** Default destructor */
+	virtual ~m3d_spot_light_source(){};
+	/** Copy constructor
+	 *  \param other Object to copy from
+	 */
+	m3d_spot_light_source(const m3d_spot_light_source &other) : m3d_point_light_source(other) {}
 
-    m3d_spot_light_source(const struct m3d_input_point &lookat,
-                          const struct m3d_input_point &position,
-                          const m3d_color &color,
-                          const float Kc,
-                          const float Kl,
-                          const float Kq,
-                          const float intensity) : m3d_point_light_source(position, color, Kc, Kl, Kq, intensity), direction(lookat)
-    {
-        m3d_vector temppos(get_position());
-        direction.subtract(temppos);
-        direction.normalize();
-    }
+	m3d_spot_light_source(const struct m3d_input_point &lookat,
+			      const struct m3d_input_point &position,
+			      const m3d_color &color,
+			      const float Kc,
+			      const float Kl,
+			      const float Kq,
+			      const float intensity) : m3d_point_light_source(position, color, Kc, Kl, Kq, intensity), direction(lookat)
+	{
+		m3d_vector temppos(get_position());
+		direction.subtract(temppos);
+		direction.normalize();
+	}
 
-    // World coordinates not rotated
-    float get_intensity(const m3d_point &objpos, const m3d_point &viewer);
+	// World coordinates not rotated
+	float get_intensity(const m3d_point &objpos, const m3d_point &viewer);
 
-    virtual void print(void);
+	virtual void print(void);
 
 protected:
-    m3d_vector direction;
+	m3d_vector direction;
 };
 
 #endif // M3D_LIGHT_SOURCE_H
