0138052a8f699a06fb2b8593da42134ca2bfac92
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2024-09-03 23:58:48 +0200

    m3d_camera: cleanup and fix frustum output

--- m3d_camera.cpp
+++ m3d_camera.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-m3d_camera::m3d_camera() : position(), transform(), frustum(0.0f, 0, 0)
+m3d_camera::m3d_camera() : position(), transform(), frustum(0.0f, 0, 0, 1.0f, INFINITY)
 {
 	screen_resolution.x = screen_resolution.y = 0;
 }
@@ -18,16 +18,19 @@ m3d_camera::~m3d_camera()
 m3d_camera::m3d_camera(const struct m3d_input_point &position,
 		       const struct m3d_input_point &at,
 		       int16_t xres,
-		       int16_t yres) : position(position), transform(position, at), frustum(45.0f, xres, yres)
+		       int16_t yres) : position(position), transform(position, at), frustum(45.0f, xres, yres, 10.0f, 1000.0f)
 {
 	screen_resolution.x = xres;
 	screen_resolution.y = yres;
 }
 
-void m3d_camera::transform_to_frustum(m3d_point &point)
+void m3d_camera::projection(m3d_point &point)
 {
 	transform.transform(point, point);
 	frustum.transform(point, point);
+	point.myvector[X_C] /= point.myvector[T_C];
+	point.myvector[Y_C] /= point.myvector[T_C];
+	point.myvector[Z_C] /= point.myvector[T_C];
 }
 
 void m3d_camera::transform_to_viewpoint(m3d_point &pointsrc, m3d_point &pointdst)
@@ -43,16 +46,18 @@ void m3d_camera::transform_to_viewpoint(m3d_vertex &vertex)
 
 void m3d_camera::project_to_screen(m3d_point &point, SDL_Point &pix)
 {
-	pix.x = (int)(point.myvector[X_C] / point.myvector[T_C] * screen_resolution.x + screen_resolution.x) / 2;
-	pix.y = (int)(-point.myvector[Y_C] / point.myvector[T_C] * screen_resolution.y + screen_resolution.y) / 2;
+	pix.x = (int)(point.myvector[X_C] * screen_resolution.x + screen_resolution.x) / 2;
+	pix.y = (int)(-point.myvector[Y_C] * screen_resolution.y + screen_resolution.y) / 2;
 }
 
 void m3d_camera::transform_and_project_to_screen(m3d_point &pointsrc, m3d_point &pointdst, SDL_Point &pix)
 {
 	transform.transform(pointsrc, pointdst);
 	frustum.transform(pointdst, pointdst);
-	pix.x = (int)(pointdst.myvector[X_C] / pointdst.myvector[T_C] * screen_resolution.x + screen_resolution.x) / 2;
-	pix.y = (int)(-pointdst.myvector[Y_C] / pointdst.myvector[T_C] * screen_resolution.y + screen_resolution.y) / 2;
+	pointdst.myvector[X_C] /= pointdst.myvector[T_C];
+	pointdst.myvector[Y_C] /= pointdst.myvector[T_C];
+	pointdst.myvector[Z_C] /= pointdst.myvector[T_C];
+	project_to_screen(pointdst, pix);
 }
 
 bool m3d_camera::is_visible(m3d_point &point, m3d_vector &normal)
--- m3d_camera.hh
+++ m3d_camera.hh
@@ -24,25 +24,25 @@ public:
 	m3d_camera(const m3d_camera &other) : position(other.position),
 					      transform(other.transform),
 					      frustum(other.frustum),
-					      screen_resolution(other.screen_resolution){};
+					      screen_resolution(other.screen_resolution) {};
 
 	m3d_camera(const struct m3d_input_point &position,
 		   const struct m3d_input_point &at,
 		   int16_t xres,
 		   int16_t yres);
 
-	// Transform point from world coordinates to view (camera) frustum coordinates
-	void transform_to_frustum(m3d_point &point);
+	// Project a world coordinates point inside the view frustum to homogeneous clip space coordinates
+	void projection(m3d_point &point);
 	// Transform point from world coordinates to camera coordinates
 	void transform_to_viewpoint(m3d_point &pointsrc, m3d_point &pointdst);
 	// Transform vertex from world coordinates to camera coordinates
 	void transform_to_viewpoint(m3d_vertex &vertexsrc);
-	// Project point from view plane to screen
+	// Project point from homogeneous clip space coordinates to screen
 	void project_to_screen(m3d_point &point, SDL_Point &pix);
-	// Transform point from world coordinates to camera and project to screen
+	// Project point from world coordinates to screen
 	void transform_and_project_to_screen(m3d_point &pointsrc, m3d_point &pointdst, SDL_Point &pix);
 	// Compute visibility using normal as a surface normal vector and point to compute
-	// the vector going from camera to point itself
+	// the vector going from camera to point itself in world coordinates.
 	bool is_visible(m3d_point &point, m3d_vector &normal);
 
 	const m3d_point &get_position(void) { return position; }
