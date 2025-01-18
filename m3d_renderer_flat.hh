7a9c5792bfee4f310de34463e8d3f34ad2d3c3e0
J-F-Sebastian <J-F-Sebastian@users.noreply.github.com>
2025-01-18 11:37:04 +0100

    m3d_renderer_flat: clean up implementation

--- m3d_renderer_flat.hh
+++ m3d_renderer_flat.hh
@@ -35,22 +35,6 @@ public:
 	virtual void render(m3d_world &world);
 
 private:
-	void triangle_fill_flat(m3d_vertex *vtx[], int16_t *runlen, m3d_color &color);
-};
-
-class m3d_renderer_flatf : public m3d_renderer
-{
-public:
-	/** Default constructor */
-	m3d_renderer_flatf() : m3d_renderer() {};
-	m3d_renderer_flatf(m3d_display *disp) : m3d_renderer(disp) {};
-
-	/** Default destructor */
-	virtual ~m3d_renderer_flatf() {};
-
-	virtual void render(m3d_world &world);
-
-private:
 	void triangle_fill_flat(m3d_vertex *vtx[], m3d_color &color);
 };
 
